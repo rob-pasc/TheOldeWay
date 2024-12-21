389bee917a28e3235ffc2d40b3ce4e91bd1c46b1
rob-pasc <robinpascal.k@gmail.com>
2024-12-21 18:29:15 +0100

    fixes?

--- frontend/public/js/service-worker.js
+++ frontend/public/js/service-worker.js
@@ -1,9 +1,14 @@
 self.addEventListener('install', event => {
-    console.log('Service worker installed.');
+    self.skipWaiting(); 
+    // console.log('Service worker installed.');
     alert('Service worker installed.')
   });
   self.addEventListener('fetch', event => {
-    console.log('Fetching:', event.request.url);
+    // console.log('Fetching:', event.request.url);
     alert('Fetching:', event.request.url);
+  });
+  self.addEventListener('activate', event => {
+    // console.log('Service worker activated.');
+    alert('Service worker activated.');
   });
   
