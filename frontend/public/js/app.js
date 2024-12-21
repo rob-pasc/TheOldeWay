2bb35ad4f9683f283ae81a4106c0bceb299715be
rob-pasc <robinpascal.k@gmail.com>
2024-12-21 17:43:27 +0100

    pwa adj

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -1,3 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js').then(
+      registration => console.log('ServiceWorker registered:', registration),
+      err => console.error('ServiceWorker registration failed:', err)
+    );
+  });
+}
 
 
 document.addEventListener("DOMContentLoaded", () => {
